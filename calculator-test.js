f4f0a66ffb471105b2e6e04e9fd3dedb594bbe8e
Peter Lombardo <peterllombardo@gmail.com>
2023-02-25 10:16:20 -0500

    Wrote tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,12 +1,19 @@
 
 it('should calculate the monthly rate correctly', function () {
     // ...
+    const values = {amount: 25000, years: 5, rate: 5.5};
+    expect(calculateMonthlyPayment(values)).toEqual('477.53');
   });
   
   
   it("should return a result with 2 decimal places", function() {
     // ..
+    const values = {amount: 100000, years: 5, rate: 8.2};
+    expect(calculateMonthlyPayment(values)).toEqual('2037.22');
   });
   
-  /// etc
+  it ("should throw an error if string is entered", function(){
+    if (values = NaN);
+    expect(calculateMonthlyPayment(values)).toThrowError("Please enter a number");
+  })
   
